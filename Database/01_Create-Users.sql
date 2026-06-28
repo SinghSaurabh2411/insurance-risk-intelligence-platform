a433ec5891ac653f7d628dcfd8754b1533258a96
Saurabh Singh <saurabhsingh.in95@gmail.com>
2026-06-28 20:37:46 +0530

    Update 01_Create-Users.sql

--- Database/01_Create-Users.sql
+++ Database/01_Create-Users.sql
@@ -1,5 +1,5 @@
 /*****************************************************************************************
- Project        : Insurance Risk Intelligence Platform
+ Project        : Healthcare Insurance Data Warehouse
  File           : 01_create_users.sql
  Layer          : Oracle Database
  Purpose        : Create schemas for Medallion Architecture
@@ -12,6 +12,7 @@
  DWH_BRONZE
  DWH_SILVER
  DWH_GOLD
+ DWH_CONTROL
 
 *****************************************************************************************/
 
@@ -31,7 +32,7 @@ CREATE BRONZE USER
 *****************************************************************************************/
 
 CREATE USER DWH_BRONZE
-IDENTIFIED BY bronze123
+IDENTIFIED BY saurabh
 DEFAULT TABLESPACE USERS
 TEMPORARY TABLESPACE TEMP
 QUOTA UNLIMITED ON USERS;
@@ -53,7 +54,7 @@ CREATE SILVER USER
 *****************************************************************************************/
 
 CREATE USER DWH_SILVER
-IDENTIFIED BY silver123
+IDENTIFIED BY saurabh
 DEFAULT TABLESPACE USERS
 TEMPORARY TABLESPACE TEMP
 QUOTA UNLIMITED ON USERS;
@@ -75,7 +76,7 @@ CREATE GOLD USER
 *****************************************************************************************/
 
 CREATE USER DWH_GOLD
-IDENTIFIED BY gold123
+IDENTIFIED BY saurabh
 DEFAULT TABLESPACE USERS
 TEMPORARY TABLESPACE TEMP
 QUOTA UNLIMITED ON USERS;
@@ -90,7 +91,47 @@ GRANT
     CREATE SYNONYM
 TO DWH_GOLD;
 
+/*
 
+*****************************************************************************************
+Create DWH Control Schema
+*****************************************************************************************/
+
+
+Description
+-----------
+Creates the schema responsible for ETL metadata, audit,
+control tables and sequences.
+
+*/
+
+CREATE USER DWH_CONTROL
+IDENTIFIED BY dwh_control
+DEFAULT TABLESPACE USERS
+TEMPORARY TABLESPACE TEMP
+QUOTA UNLIMITED ON USERS;
+
+------------------------------------------------------------
+-- Required Privileges
+------------------------------------------------------------
+
+GRANT CREATE SESSION TO DWH_CONTROL;
+
+GRANT CREATE TABLE TO DWH_CONTROL;
+
+GRANT CREATE VIEW TO DWH_CONTROL;
+
+GRANT CREATE SEQUENCE TO DWH_CONTROL;
+
+GRANT CREATE PROCEDURE TO DWH_CONTROL;
+
+GRANT CREATE TRIGGER TO DWH_CONTROL;
+
+GRANT CREATE SYNONYM TO DWH_CONTROL;
+
+GRANT UNLIMITED TABLESPACE TO DWH_CONTROL;
+
+COMMIT;
 
 /*****************************************************************************************
 END OF SCRIPT
